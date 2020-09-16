96f95df0afef3934759ab8d8d6371236ab485a20
Yetian <yetian.mao@gmail.com>
2020-09-16 14:17:15 +0900

    feat: add background layer to randomize (#4)

--- src/components/BackgroundOverlay.tsx
+++ src/components/BackgroundOverlay.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import SVG from "react-inlinesvg";
+import { BackgroundOverlays } from "../utils/assets";
+import { getRandomInt } from "../utils/random";
 
-const StyledSvg = styled(SVG)<{ color?: string; transform?: string; }>`
+const StyledSvg = styled(SVG)<{ color?: string; transform?: { x: number, y: number }; }>`
+  position: absolute;
+  left: 0;
+  top: 0;
   fill: ${(props) => props.color};
   path {
-    transform: ${(props) => props.transform};
-  }
+    transform: ${(props) => `translate(${props.transform!.y}px, ${props.transform!.x}px)` };
+  };
 `;
 
 interface Props {
-  svg: string;
   color?: string;
-  transform?: string;
 }
 
-export default ({ svg, color, transform }: Props) => {
+const verticalTransformRange = [5, 20]
+const horiztonalTransformRange = [-50, 50]
+
+export default ({ color }: Props) => {
+  const index = getRandomInt(0, BackgroundOverlays.length);
+  const svg = BackgroundOverlays[index].src;
+  const defaultTransform = BackgroundOverlays[index].defaultTransform;
+  const transform = {
+    x: defaultTransform.x + getRandomInt(horiztonalTransformRange[0], horiztonalTransformRange[1]),
+    y: defaultTransform.y + getRandomInt(verticalTransformRange[0], verticalTransformRange[1])
+  }
   return <StyledSvg src={svg} color={color} transform={transform} />;
 };
